9fb5173c05b823b7ad2a49780f06e4d893d3750c
wuyue <yy107829@sina.com>
2015-10-12 17:43:13 +0800

    添加

---
+++ src/com/calm/calm/db/DataHelper.java
@@ -0,0 +1,31 @@
+package com.calm.calm.db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+
+import com.j256.ormlite.android.apptools.OrmLiteSqliteOpenHelper;
+import com.j256.ormlite.support.ConnectionSource;
+
+public class DataHelper extends OrmLiteSqliteOpenHelper{
+
+	public DataHelper(Context context, String databaseName,
+			CursorFactory factory, int databaseVersion) {
+		super(context, databaseName, factory, databaseVersion);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase arg0, ConnectionSource arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase arg0, ConnectionSource arg1, int arg2,
+			int arg3) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
